7edc7bfc08913cb33cd492776c9131df10584da2
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-06-18 00:07:50 -0300

    feat: 1° imp controller

---
+++ controller/MoradorController.py
@@ -0,0 +1,10 @@
+from model import Morador
+from view import CadastrarMoradorView
+
+    class MoradorController:
+        def __init__(self):
+            self.morador[]
+            
+        def cadastrar_morador(self, idmorador, nomemorador, cpfmorador, emailmorador, senhamorador, telefonemorador):
+            morador = Morador(idmorador, nomemorador, cpfmorador, emailmorador, senhamorador, telefonemorador)
+            
